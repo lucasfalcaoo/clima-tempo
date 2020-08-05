164a63de4d57353330be30d47ce2f73f833eacf0
lucas_falcao <lucas.falcao.dsg@gmail.com>
2020-08-05 14:39:34 -0300

    Initial home page structure

---
+++ src/pages/Home/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wrapper from '../../../components/Wrapper'
+import { Title } from './styles';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Title>
+        Olá!
+      </Title>
+    </Wrapper>
+  )
+}
---
+++ src/pages/Home/components/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { View, Text } from 'react-native';
+
+export const Title = styled.Text`
+  font-size: 24px;
+  text-align: center;
+`;
